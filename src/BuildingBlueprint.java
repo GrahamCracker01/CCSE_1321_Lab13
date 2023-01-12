8141aff64e5bd19273e76383c6661903c8db2c32
GrahamCracker01 <mrgrahamcracker01@gmail.com>
2023-01-12 18:45:10 -0500

    Initial Commit

--- src/BuildingBlueprint.java
+++ src/BuildingBlueprint.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class BuildingBlueprint {
+//Class: CSE 1321L
+//Section: J02
+///Term: Fall 2022
+//Instructor: Aarthi Poovalingam
+//Name: Asher Graham
+//Lab#: ...
+
+import java.util.Scanner;
+class BuildingBlueprint {
+
+    //declare variables
+    private int intStories;
+    private int intApart;
+    private float fltOcc;
+    private boolean boolFull;
+
+    //constructors
+    public  BuildingBlueprint() {
+        intStories = 10;
+        intApart = 20;
+        fltOcc = 1;
+        boolFull = true;
+    }
+    public BuildingBlueprint(int a, int b, float c) {
+        intStories = a;
+        intApart = b;
+        fltOcc = c;
+        if (fltOcc < 1) {
+            boolFull = false;
+        }
+        else {
+            boolFull = true;
+        }
+
+    }
+
+    //setter and getters
+    public void setFltOcc(float a) {
+        if (a >= 0 && a <= 1) {
+            fltOcc = a;
+        }
+        if (fltOcc < 1) {
+            boolFull = false;
+        }
+        else {
+            boolFull = true;
+        }
+    }
+
+    public float getFltOcc() {
+        return fltOcc;
+    }
+
+    public int getIntStories() {
+        return intStories;
+    }
+
+    public int getIntApart() {
+        return intApart;
+    }
+
+    public boolean getBoolFull() {
+        return boolFull;
+    }
 }
--- src/Lab13A.java
+++ src/Lab13A.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Lab13A {
+//Class: CSE 1321L
+//Section: J02
+///Term: Fall 2022
+//Instructor: Aarthi Poovalingam
+//Name: Asher Graham
+//Lab#: ...
+
+class Lab13A {
+    public static void main(String[] args){
+
+        //create buildings
+        BuildingBlueprint building1 = new BuildingBlueprint();
+        BuildingBlueprint building2 = new BuildingBlueprint(30, 30, (float)0.75);
+
+        System.out.println("Year 2020:");
+        System.out.println("Building 1 has " + building1.getIntStories() + " floors, " + building1.getIntApart() + " apartments, and is " + (int) (building1.getFltOcc() * 100) + "% occupied. Full? " + building1.getBoolFull());
+        System.out.println("Building 2 has " + building2.getIntStories() + " floors, " + building2.getIntApart() + " apartments, and is " + (int) (building2.getFltOcc() * 100) + "% occupied. Full? " + building2.getBoolFull());
+        System.out.println("Many years pass.");
+
+        //set new occupation and final output
+        building1.setFltOcc(0);
+        building2.setFltOcc(1);
+
+        System.out.println("Year 2043:");
+        System.out.println("Building 1 has " + building1.getIntStories() + " floors, " + building1.getIntApart() + " apartments, and is " + (int) (building1.getFltOcc() * 100) + "% occupied. Full? " + building1.getBoolFull());
+        System.out.println("Building 2 has " + building2.getIntStories() + " floors, " + building2.getIntApart() + " apartments, and is " + (int) (building2.getFltOcc() * 100) + "% occupied. Full? " + building2.getBoolFull());
+        System.out.println("Looks like people prefer taller buildings.");
+
+    }
 }
